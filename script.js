53e05a5835d8944920ee0aa6e8759d435e8fec08
Breno Bader <81591179+brenobader@users.noreply.github.com>
2022-01-28 11:31:51 -0300

    Inicia o projeto index e script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+// Crie uma variável que receba um nome;
+
+// Crie uma variável que receba um número;
+
+// Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+
+// Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+
+//Mostre no console a mensagem "Estou mexendo no console.log";
+
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console;
+
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+
+let name= 'Bader Zu';
+let number= 26;
+let happynew= 2022 - number;
+
+console.log(happynew + number);
+
+console.log('Estou mexendo no console');   
+
+let quartaFeira = true;
+
+console.log(typeof quartaFeira);
+
+ 
